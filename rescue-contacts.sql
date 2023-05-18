3893ff99a972a895d6684c0b09d4cce618b89e7b
Patrick Schumacher <bahoop@gmx.net>
2023-05-18 20:07:18 +0200

    Update rescue-contacts.sql

--- rescue-contacts.sql
+++ rescue-contacts.sql
@@ -1,4 +1,5 @@
 -- extract all vcf cards-entries from an adressbook. just store all entries with linebreaks as *.vcf file and your done
+-- caution: sql in statement is used, ensure manually that the inner like query return only one row as a result!
 select carddata
 from oc_cards
 where addressbookid in
